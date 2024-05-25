1ba45ef4430aadcc5410a94d24c19fca3c10f2eb
cacobaco <lcjmonizf09@gmail.com>
2024-05-25 18:56:15 +0100

    style: reformat with prettier and use 2 spaces tabs

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,17 @@
 /**
+ * Converts RGB color values to a hexadecimal color representation.
+ *
+ * @param {number} r - The red color value (0-255).
+ * @param {number} g - The green color value (0-255).
+ * @param {number} b - The blue color value (0-255).
+ *
+ * @returns {number} The hexadecimal color representation.
+ */
+function rgbToHex(r, g, b) {
+  return (r << 16) + (g << 8) + b;
+}
+
+/**
  * Displays the error modal with the specified title and message.
  *
  * @param {string} title - The title of the error modal.
